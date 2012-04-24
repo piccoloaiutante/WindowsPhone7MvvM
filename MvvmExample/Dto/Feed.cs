8ae9c8cd7390d7f0462f97a904319f52f0afc7a7
Michele Capra <michele@orangecode.it>
2012-04-24 10:43:45 +0200

    Added INotifyPropertyChanged & REST example

---
+++ MvvmExample/Dto/Feed.cs
@@ -0,0 +1,12 @@
+﻿namespace MvvmExample.Dto
+{
+    public class Item
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public string Author { get; set; }
+        public string Comments { get; set; }
+        public string PubDate { get; set; }
+    }
+}
--- MvvmExample/ViewModel/MainViewModel.cs
+++ MvvmExample/ViewModel/MainViewModel.cs
@@ -1,20 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using MvvmExample.Dto;
 using MvvmExample.Infrastrucutre;
+using RestSharp;
 
 namespace MvvmExample.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
-        public DelegateCommand OpenDetailCommand { get; set; }
+        private IList<Item> _posts;
+
+        public IList<Item> Posts
+        {
+            get { return _posts; }
+        }
+        public DelegateCommand OpenDetailCommand { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public MainViewModel()
         {
             OpenDetailCommand = new DelegateCommand(ExecuteOpenDetailCommand);
+
+            LoadFeed(@"http://www.webdebs.org/category/eventi/feed/");
         }
 
         private void ExecuteOpenDetailCommand()
         {
-            App.Container.Resolve<INavigationService>().NavigateTo(new Uri("/View/DetailView.xaml",UriKind.RelativeOrAbsolute));
+            App.Container.Resolve<INavigationService>().NavigateTo(new Uri("/View/DetailView.xaml", UriKind.RelativeOrAbsolute));
+            
         }
+
+        private void LoadFeed(string url)
+        {
+            RestClient client = new RestClient
+            {
+                BaseUrl = url
+            };
+            var request = new RestRequest { RequestFormat = DataFormat.Xml };
+
+            client.ExecuteAsync<List<Item>>(request, response =>
+                                                         {
+                                                        
+                _posts = response.Data;
+                                                             OnPropertyChanged("Posts");
+            });
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        
     }
 }
